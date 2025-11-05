8b83eaad6cac1c4692c5875714139e1b46852267
Tyler <ty@zzox.net>
2025-11-05 15:05:30 -0600

    large map

--- src/game/data/Maps.hx
+++ src/game/data/Maps.hx
@@ -1,6 +1,32 @@
 package game.data;
 
 final map1 = '
+-----------------oEo--------------oXo
+-----------------ooo--------------ooo
+-----------------ooo--------------ooo
+-----------------ooo---oooooooooooooo
+-----------------ooo---DooDooDooDooDo
+-----------------ooo---oooooooooooooo
+-----------------ooo---oooooooooooooo
+-----------------ooo---oooooooooooooo
+-----------------ooo--------------ooo
+-----------------ooo--------------ooo
+-ooooooooooooooooooo--------------ooo
+-ooooooooooooooooooo--------------ooo
+-ooooooooooooooooooo--------------ooo
+-ooooooooooooooooooo--------------ooo
+-ooooooooooooooooooo--------------ooo
+-ooooooooooooooooooo--------------ooo
+-ooooooooooooooooooo--------------ooo
+-----------------ooo--------------ooo
+-----------------ooo--------------ooo
+-----------------ooo--------------ooo
+-----------------oooooooooooooooooooo
+-----------------oooooooooooooooooooo
+-----------------oooooooooooooooooooo
+';
+
+final map1Old = '
 oooEoooooooo
 Dooooooooooo
 oo---oo---oo
