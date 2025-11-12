5e91e07369836da7f1ccd5395e4337d3c2b56854
zzox <ty@zzox.net>
2025-11-11 22:01:01 -0600

    start of ui window

--- src/game/scenes/UiScene.hx
+++ src/game/scenes/UiScene.hx
@@ -6,9 +6,11 @@ import core.gameobjects.NineSlice;
 import core.scene.Scene;
 import game.ui.UiElement;
 import game.ui.UiText;
+import game.ui.UiWindow;
 import game.util.TextUtil;
 import game.world.World;
 import kha.Assets;
+import kha.graphics2.Graphics;
 
 class UiScene extends Scene {
     var world:World;
@@ -20,6 +22,9 @@ class UiScene extends Scene {
     var middleText:BitmapText;
     var middleSubtext:BitmapText;
 
+    var windows:Array<UiWindow> = [];
+
+    // TEMP:
     var el:UiElement;
     var ct:Int = 0;
 
@@ -40,9 +45,13 @@ class UiScene extends Scene {
         entities.push(middleSubtext = makeBitmapText(0, 80, ''));
 
         // entities.push(nineSlice = new NineSlice(0, 0, 16, 16, 3, 3, 13, 13, 250, 100, Assets.images.ui));
-        entities.push(el = new UiElement(0, 0, 16, 16, 3, 3, 13, 13, 250, 100, Assets.images.ui, () -> {
-            ct++;
-        }));
+        // entities.push(el = new UiElement(0, 0, 16, 16, 3, 3, 13, 13, 250, 100, Assets.images.ui, () -> {
+        //     ct++;
+        // }));
+
+        final window = new UiWindow(0, 0);
+        windows.push(window);
+        entities.push(window);
 
         for (i in 0...8) {
             final text = makeBitmapText(4, 100 + i * 10, '');
@@ -63,8 +72,11 @@ class UiScene extends Scene {
         middleText.setPosition(Math.floor((camera.width / camera.scale - middleText.textWidth) / 2), middleText.y);
         middleText.visible = middleTextTime > 0;
 
-        el.checkPointer(Math.floor(Game.mouse.position.x / camera.scale), Math.floor(Game.mouse.position.y / camera.scale));
-        el.setIndexFromState();
+        // for every button
+        // el.checkPointer(Math.floor(Game.mouse.position.x / camera.scale), Math.floor(Game.mouse.position.y / camera.scale));
+        // el.setIndexFromState();
+
+        // HACK:
         devTexts[devTexts.length - 1].setText(ct + '');
     }
 
@@ -72,4 +84,9 @@ class UiScene extends Scene {
         middleText.setText(text);
         middleTextTime = time;
     }
+
+    // override function render (g2:Graphics, clears:Bool) {
+    //     super.render(g2, clears);
+    //     for (w in windows) w.render(g2, camera);
+    // }
 }
---
+++ src/game/ui/UiWindow.hx
@@ -0,0 +1,42 @@
+package game.ui;
+
+import core.gameobjects.GameObject;
+import core.system.Camera;
+import kha.Assets;
+import kha.graphics2.Graphics;
+
+typedef ChildElements = {
+    var el:GameObject;
+    var x:Int;
+    var y:Int;
+}
+
+// a collection of gameobjects all rendered to a relative position
+class UiWindow extends GameObject {
+    // public var x:Int;
+    // public var y:Int;
+
+    public var children:Array<ChildElements> = [];
+    public var grabbable:Null<UiElement>;
+    public var cancel:Null<UiElement>;
+
+    public function new (x:Int, y:Int) {
+
+        final topbar = new UiElement(0, 0, 16, 16, 3, 3, 13, 13, 100, 16, Assets.images.ui);
+        final bottomBg = new UiElement(0, 0, 16, 16, 3, 3, 13, 13, 100, 100, Assets.images.ui);
+
+        children = [{ x: 0, y: 0, el: topbar }, { x: 0, y: 16, el: bottomBg }];
+
+        grabbable = topbar;
+    }
+
+    override function update (delta:Float) {}
+
+    override function render (g2:Graphics, cam:Camera) {
+        for (c in children) {
+            c.el.x = x + c.x;
+            c.el.y = y + c.y;
+            c.el.render(g2, cam);
+        }
+    }
+}
